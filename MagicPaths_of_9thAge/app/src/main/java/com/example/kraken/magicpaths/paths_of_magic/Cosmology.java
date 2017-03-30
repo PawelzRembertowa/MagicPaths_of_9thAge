12e4f29596409baf847901d899a42ecb32075315
Pawel <dragon80@wp.pl>
2017-03-30 15:28:03 +0200

    first cosmology spell added

--- MagicPaths_of_9thAge/app/src/main/java/com/example/kraken/magicpaths/paths_of_magic/Cosmology.java
+++ MagicPaths_of_9thAge/app/src/main/java/com/example/kraken/magicpaths/paths_of_magic/Cosmology.java
@@ -22,17 +22,18 @@ public class Cosmology extends AppCompatActivity {
     public void addAlchemyToDB() {
 
         cosmologyItem1 = new ContentValues();
-        cosmologyItem1.put(SpellsTableContract.COL_SPELL_NUMBER, "A");
-        cosmologyItem1.put(SpellsTableContract.COL_SPELL_NAME, "Alchemical Fire");
-        cosmologyItem1.put(SpellsTableContract.COL_SPELL_VALUE, " ");
-        cosmologyItem1.put(SpellsTableContract.COL_SPELL_RANGE, "18''");
-        cosmologyItem1.put(SpellsTableContract.COL_SPELL_ALL_TYPE, "Hex");
-        cosmologyItem1.put(SpellsTableContract.COL_SPELL_COSMOS_TYPE, "Hex");
-        cosmologyItem1.put(SpellsTableContract.COL_SPELL_CHAOS_TYPE, "Hex");
-        cosmologyItem1.put(SpellsTableContract.COL_SPELL_COSMOS_DURATION, "Lasts one Turn");
-        cosmologyItem1.put(SpellsTableContract.COL_SPELL_CHAOS_DURATION, "Hex");
-        cosmologyItem1.put(SpellsTableContract.COL_SPELL_COSMOS_EFFECT, "The target gains Flammable against Close Combat Attacks and Spells.");
-        cosmologyItem1.put(SpellsTableContract.COL_SPELL_CHAOS_EFFECT, "The target gains Flammable against Close Combat Attacks and Spells.");
+        cosmologyItem1.put(SpellsTableContract.COL_SPELL_NUMBER, "0");
+        cosmologyItem1.put(SpellsTableContract.COL_SPELL_NAME, "Altered Sight");
+        cosmologyItem1.put(SpellsTableContract.COL_SPELL_VALUE, "7+ <font color=#802000>{5+}</font>");
+        cosmologyItem1.put(SpellsTableContract.COL_SPELL_RANGE, "24''");
+        cosmologyItem1.put(SpellsTableContract.COL_SPELL_ALL_TYPE, " ");
+        cosmologyItem1.put(SpellsTableContract.COL_SPELL_COSMOS_TYPE, "<b>Augment</b>");
+        cosmologyItem1.put(SpellsTableContract.COL_SPELL_CHAOS_TYPE, "<b>Hex</b>");
+        cosmologyItem1.put(SpellsTableContract.COL_SPELL_DURATION, "Lasts one Turn");
+        cosmologyItem1.put(SpellsTableContract.COL_SPELL_COSMOS_DURATION, " ");
+        cosmologyItem1.put(SpellsTableContract.COL_SPELL_CHAOS_DURATION, " ");
+        cosmologyItem1.put(SpellsTableContract.COL_SPELL_COSMOS_EFFECT, "The target gains <b>+1</b> Weapon Skill and <b>+1</b> Ballistic Skill.");
+        cosmologyItem1.put(SpellsTableContract.COL_SPELL_CHAOS_EFFECT, "The target suffers <b>-1</b> Weapon Skill, to a minimum of 1, and <b>-1</b> Ballistic Skill.");
     }
 
 
